24ff851817e7b1df221f9841b3b4d34d5197d16f
Orlane2905 <orlanedjuidje748@gmail.com>
2025-11-17 16:41:29 +0100

    Ajout d'une deuxième ligne au message de A

--- src/MessageA.java
+++ src/MessageA.java
@@ -1,7 +1,8 @@
 public class MessageA {
 	
 public static void main(String[] args) {
-System.out.println("Message du membre A");
+		System.out.println("Message du membre A");
+		System.out.println("Deuxième ligne de A");
 
 }
 }
